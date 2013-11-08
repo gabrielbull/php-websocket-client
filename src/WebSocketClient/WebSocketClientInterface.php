68c1451275592bf113c1b386c45f5cdf667d111f
Gabriel Bull <gavroche.bull@gmail.com>
2013-11-07 21:45:13 -0500

    Removed return PHPdoc from Websocket Interface

--- src/WebSocketClient/WebSocketClientInterface.php
+++ src/WebSocketClient/WebSocketClientInterface.php
@@ -8,20 +8,17 @@ interface WebSocketClientInterface
 {
     /**
      * @param array $data
-     * @return mixed
      */
     function onWelcome(array $data);
 
     /**
      * @param string $topic
      * @param string $event
-     * @return mixed
      */
     public function onEvent($topic, $event);
 
     /**
      * @param WebSocketClient $client
-     * @return mixed
      */
     function setClient(WebSocketClient $client);
 }
